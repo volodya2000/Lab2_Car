7054ae35dc8f47d084c1e59bd36fae63fac350a9
volodya2000 <vovan.zhuk3.14@gmail.com>
2020-09-29 17:41:09 +0300

    create Car class

--- src/Color.java
+++ src/Color.java
@@ -8,7 +8,7 @@ public enum Color {
             List.of(values());
     private static final int SIZE = COLORS.size();
     private static final Random RANDOM = new Random();
-    
+
     public static Color randomColor()  {
         return COLORS.get(RANDOM.nextInt(SIZE));
     }
